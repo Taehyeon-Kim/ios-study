4699b4d03c1644fbed5ba2aeb9b76c4ac56c12d5
Taehyeon-Kim <taehyk528@gmail.com>
2020-10-20 12:53:25 +0900

    [FORMAT] 코드 포맷 수정

--- Autolayout/autolayout01-TableView/autolayout01/ViewController.swift
+++ Autolayout/autolayout01-TableView/autolayout01/ViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
     var contents = [
+        "드디어 어떤 것도 참고하지 않고, 화면 레이아웃 대로 테이블 뷰를 만들어냈다. 완벽한 코드는 아니지만 잘 동작한다. 👨🏻‍💻🔥🔥",
         "simply dummy text of the printing and",
         "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.",
         "Lorem ipsum dolor sit amet.",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec fermentum quam, id vehicula lacus. Nullam cursus tortor et mollis.",
-        "드디어 어떤 것도 참고하지 않고, 화면 레이아웃 대로 테이블 뷰를 만들어냈다.",
         "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.",
         "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
         "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",

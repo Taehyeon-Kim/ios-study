72335762644ef48470a20402055932cff83b49ab
Taehyeon-Kim <taehyk528@gmail.com>
2020-10-30 13:35:27 +0900

    [CHORE] 텍스트뷰 placeholder 프로젝트 생성

---
+++ textview-placeholder/textview-placeholder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  textview-placeholder
+//
+//  Created by taehy.k on 2020/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

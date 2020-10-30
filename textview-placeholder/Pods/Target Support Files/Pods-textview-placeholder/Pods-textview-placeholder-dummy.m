311f0b0cbcbc7a8eed19143ff823507b51f729cd
Taehyeon-Kim <taehyk528@gmail.com>
2020-10-30 13:44:48 +0900

    [FEAT] 텍스트뷰 placeholder 오픈소스 사용

---
+++ textview-placeholder/Pods/Target Support Files/Pods-textview-placeholder/Pods-textview-placeholder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_textview_placeholder : NSObject
+@end
+@implementation PodsDummy_Pods_textview_placeholder
+@end
--- textview-placeholder/textview-placeholder/ViewController.swift
+++ textview-placeholder/textview-placeholder/ViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import UITextView_Placeholder
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myTextView.placeholder = "이것은 텍스트뷰의 placeholder입니다!"
+        myTextView.placeholderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
     }
 
 
